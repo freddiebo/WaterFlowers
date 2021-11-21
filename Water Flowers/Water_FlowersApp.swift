41a0948d09a0cdc0ccb9de78b879c0f70f8f5aeb
vsbondarev <vsbondarev@fil-it.ru>
2021-11-21 13:07:54 +0300

    Initial Commit

---
+++ Water Flowers/Water_FlowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_FlowersApp.swift
+//  Water Flowers
+//
+//  Created by Vladislav Bondarev on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Water_FlowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
